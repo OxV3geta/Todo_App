dd7d44284c2e3266a9156042ff045e2b019d5ce5
oxv3geta <mesbahsezan@gmail.com>
2026-03-14 17:27:02 +0600

    Implement the add button and fix the errors

--- app1/gui.py
+++ app1/gui.py
@@ -1,8 +1,26 @@
 import functions
 import FreeSimpleGUI as app
 text_box = app.Text("Type in a to-do")
-input_box = app.InputText(tooltip="Enter to-do")
+input_box = app.InputText(tooltip="Enter to-do",key= "todo")
 add_button = app.Button("Add")
-window = app.Window("To-Do App", layout=[[text_box],[input_box, add_button]])
-window.read()
+window = app.Window("To-Do App",
+                    layout=[[text_box],[input_box, add_button]] ,
+                    font = ("helvetica", 20 ))
+while True:
+    event ,value = window.read()
+    print(event)
+    print(value)
+    match event:
+        case 'Add':
+           todos = functions.todos_read()
+           new_todos = value["todo"] + '\n'
+           todos.append(new_todos)
+           functions.todos_write(todos)
+        case app.WIN_CLOSED:
+            break
+
+
+
+
+
 window.close()
